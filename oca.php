fa64969322abf24fe84bd242688bb7d61bc86742
craiem <mariano@bullpix.com>
2013-12-11 15:42:24 -0200

    Function IngresoOR
    $paquetesarray[] = array('alto'=>'11','ancho'=>'11','largo'=>'11','peso'=>'0.200','valor'=>'100','cant'=>'1');
    
    $IngresoOR = $oca->IngresoOR($ConfirmarRetiro = 'true',
    							 $DiasRetiro = '1',
    							 $FranjaHoraria = '1', //Debe indicar una franja horaria válida ([1]- de 8 a 17, [2]- de 8 a 12, [3]- de 14 a 17)
    							 $operativa = 'estandar',
    							 $nroremito = '123',
    							 $apellido = 'Craiem',
    							 $nombre = 'Mariano',
    							 $direccionenvio = array('calle'=>'Olleros','nro'=>'2411','piso'=>'3','depto'=>'4','cp'=>'1426','localidad'=>'Capital Federal','provincia'=>'Buenos Aires'),
    							 $email = 'mariano@bullpix.com',
    							 $observaciones = '',
    							 $paquetes = $paquetesarray);
    print_r( $IngresoOR);
    
    $tracking = $oca->Tracking_Pieza($IngresoOR[0]['NumeroEnvio']);
    print_r($tracking);

--- oca.php
+++ oca.php
@@ -2,8 +2,77 @@
 
 class Oca
 {
-	protected $webservice_url = 'webservice.oca.com.ar';
-	
+	protected $webservice_url = 'http://webservice.oca.com.ar';
+
+	function __construct($user,$pass,$cuit,$nrocuenta,$userdireccion,$centrocosto,$operativas){
+            $this->usr = $user;
+            $this->psw = $pass;
+            $this->cuit = $cuit;
+            $this->nrocuenta = $nrocuenta;
+            $this->userdireccion = $userdireccion;
+            $this->centrocosto = $centrocosto;
+            $this->operativas = $operativas;
+
+            if(!$this->usr){
+	            trigger_error('User error');
+            }
+            if(!$this->psw){
+	            trigger_error('Password error');
+            }
+    }
+
+	// =========================================================================
+
+	public function IngresoOR( $ConfirmarRetiro,$DiasRetiro,$FranjaHoraria,$operativa,$nroremito,$apellido,$nombre,$direccionenvio,$email,$observaciones,$paquetes)
+	{
+		//armo XML
+		$xml = '<ROWS>
+				<cabecera ver="1.0" nrocuenta="'.$this->nrocuenta.'" />
+				<retiro calle="'.$this->userdireccion['calle'].'" nro="'.$this->userdireccion['nro'].'" piso="'.$this->userdireccion['piso'].'" depto="'.$this->userdireccion['depto'].'" cp="'.$this->userdireccion['cp'].'" localidad="'.$this->userdireccion['localidad'].'" provincia="'.$this->userdireccion['provincia'].'" contacto="'.$this->userdireccion['solicitante'].'" email="'.$this->userdireccion['email'].'" solicitante="'.$this->userdireccion['solicitante'].'" observaciones="" centrocosto="0" />
+				<envios>
+					<envio idoperativa="'.$this->operativas[$operativa].'" nroremito="'.$nroremito.'">
+						<destinatario apellido="'.$apellido.'" nombre="'.$nombre.'" calle="'.$direccionenvio['calle'].'" nro="'.$direccionenvio['nro'].'" piso="'.$direccionenvio['piso'].'" depto="'.$direccionenvio['depto'].'" cp="'.$direccionenvio['cp'].'" localidad="'.$direccionenvio['localidad'].'" provincia="'.$direccionenvio['provincia'].'" telefono="" email="'.$email.'" idci="1" celular="" observaciones="'.$observaciones.'"/>
+						<paquetes>';
+						foreach ($paquetes as $paquete) {
+							$xml .= '<paquete alto="'.$paquete['alto'].'" ancho="'.$paquete['ancho'].'" largo="'.$paquete['largo'].'" peso="'.$paquete['peso'].'" valor="'.$paquete['valor'].'" cant="'.$paquete['cant'].'" />';
+						}
+				$xml.='</paquetes>
+					</envio>
+				</envios>
+			</ROWS>';
+
+
+		
+		$ch = curl_init();
+		$postfields = array('usr'=>$this->usr,'psw'=>$this->psw,'XML_Retiro'=>$xml,'ConfirmarRetiro'=>$ConfirmarRetiro,'DiasRetiro'=>$DiasRetiro,'FranjaHoraria'=>$FranjaHoraria);
+
+		$url = "{$this->webservice_url}/oep_tracking/Oep_Track.asmx/IngresoOR";
+
+		curl_setopt_array($ch,	array(	CURLOPT_RETURNTRANSFER	=> TRUE,
+										CURLOPT_URL				=> $url,
+										CURLOPT_POST			=> TRUE,
+										CURLOPT_RETURNTRANSFER	=> TRUE,
+										CURLOPT_POSTFIELDS		=> http_build_query($postfields)));
+
+		$result = curl_exec($ch);
+
+		$dom = new DOMDocument();
+		$dom->loadXML($result);
+
+
+		$c_imp[] = array(	'CodigoOperacion'	=> $dom->getElementsByTagName('CodigoOperacion')->item(0)->nodeValue,
+							'FechaIngreso'		=> $dom->getElementsByTagName('FechaIngreso')->item(0)->nodeValue,
+							'mailUsuario'		=> $dom->getElementsByTagName('mailUsuario')->item(0)->nodeValue,
+							'OrdenRetiro'		=> $dom->getElementsByTagName('OrdenRetiro')->item(0)->nodeValue,
+							'NumeroEnvio'		=> $dom->getElementsByTagName('NumeroEnvio')->item(0)->nodeValue,
+							'Remito'			=> $dom->getElementsByTagName('Remito')->item(0)->nodeValue,
+							'Estado'			=> $dom->getElementsByTagName('Estado')->item(0)->nodeValue,
+							'Operativa'			=> $dom->getElementsByTagName('Operativa')->item(0)->nodeValue,
+							'Etiqueta'			=> '<a href="https://www1.oca.com.ar/ocaepak/Envios/EtiquetasCliente.asp?IdOrdenRetiro='.$dom->getElementsByTagName('OrdenRetiro')->item(0)->nodeValue.'&CUIT='.$this->cuit.'" target="_blank">Imprimir Etiqueta</a>'
+							);
+		return $c_imp;
+	}
+
 	// =========================================================================
 
 	public function List_Envios($cuit = NULL,$FechaDesde = NULL, $FechaHasta = NULL)
@@ -19,6 +88,8 @@ class Oca
 										CURLOPT_POSTFIELDS		=> 'CUIT='.$cuit.'&FechaDesde='.$FechaDesde.'&FechaHasta='.$FechaHasta,
 										CURLOPT_URL				=> "{$this->webservice_url}/oep_tracking/Oep_Track.asmx/List_Envios",
 										CURLOPT_FOLLOWLOCATION	=> TRUE));
+
+		
 
 		$dom = new DOMDocument();
 		@$dom->loadXML(curl_exec($ch));
@@ -81,6 +152,43 @@ class Oca
 								'Provincia'				=> $ci->getElementsByTagName('Provincia')->item(0)->nodeValue,
 								'CodigoPostal'			=> $ci->getElementsByTagName('CodigoPostal')->item(0)->nodeValue
 							);
+		}
+		
+		return $c_imp;
+	}
+	// =========================================================================
+	
+	/**
+	 * Devuelve todos los Centros de Imposición existentes cercanos al CP
+	 * 
+	 * @param integer $CP Código Postal
+	 * @return type 
+	 */
+	public function Tracking_Pieza($NumeroEnvio)
+	{
+		if ( ! $NumeroEnvio) return;
+
+		$ch = curl_init();
+		
+		curl_setopt_array($ch,	array(	CURLOPT_RETURNTRANSFER	=> TRUE,
+										CURLOPT_HEADER			=> FALSE,
+										CURLOPT_CONNECTTIMEOUT	=> 5,
+										CURLOPT_POST			=> TRUE,
+										CURLOPT_POSTFIELDS		=> 'pieza='.$NumeroEnvio.'&CUIT='.$this->cuit.'&NroDocumentoCliente=',
+										CURLOPT_URL				=> "{$this->webservice_url}/oep_tracking/Oep_Track.asmx/Tracking_Pieza",
+										CURLOPT_FOLLOWLOCATION	=> TRUE));
+		$result =curl_exec($ch);
+		$dom = new DOMDocument();
+		@$dom->loadXML($result);
+		$xpath = new DOMXpath($dom);
+	
+		$c_imp = array();
+		foreach (@$xpath->query("//NewDataSet/Table") as $ci)
+		{
+			$c_imp[] = array(	'Descripcion_Motivo'	=> $ci->getElementsByTagName('Descripcion_Motivo')->item(0)->nodeValue,
+								'Desdcripcion_Estado'	=> $ci->getElementsByTagName('Desdcripcion_Estado')->item(0)->nodeValue,
+								'SUC'					=> $ci->getElementsByTagName('SUC')->item(0)->nodeValue,
+								'fecha'					=> $ci->getElementsByTagName('fecha')->item(0)->nodeValue);
 		}
 		
 		return $c_imp;
